541ba715f14f85491bb738cc42a5b9cc8069002c
VINAY21HCL <vinaykumar-g@hcl.com>
2024-05-25 15:08:55 +0530

    first commit

---
+++ EmployeeParkingSpots/src/main/java/com/hcl/employee/EmployeeParkingSpotsApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeParkingSpotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeParkingSpotsApplication.class, args);
+	}
+
+}
